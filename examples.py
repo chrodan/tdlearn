pep8 cleanup + gradient descent example fix

examples.py
@@ -13,12 +13,13 @@ import matplotlib.animation as animation
 
 import matplotlib.pyplot as plt
 
+
 class MiniLQMDP(mdp.LQRMDP):
     """
     toy LQR MDP
     """
 
-    def __init__(self, dt= .01, sigma=0.1):
+    def __init__(self, dt=.01, sigma=0.1):
         """
         mass: point mass of the pendulum
         length: length of the stick / pendulum
@@ -26,14 +27,14 @@ class MiniLQMDP(mdp.LQRMDP):
         dt: time step
         """
         A = np.array([[1., dt],
-                  [0., 1.]])
-        B = np.array([0., dt]).reshape(2,1)
+                      [0., 1.]])
+        B = np.array([0., dt]).reshape(2, 1)
         Q = np.diag([-1., 0.])
         #terminal_f = lambda x: np.abs(x[0]) > 10
-        
-        R = np.ones((1,1))*(-1)
 
-        mdp.LQRMDP.__init__(self, A, B, Q, R, sigma, 
+        R = np.ones((1, 1)) * (-1)
+
+        mdp.LQRMDP.__init__(self, A, B, Q, R, sigma,
                             start=np.array([0.0001, 0]))
 
 
@@ -48,38 +49,40 @@ class NLinkPendulumMDP(mdp.LQRMDP):
     def __init__(self, masses, lengths, dt=.01, sigma=0.01, penalty=0.01, action_penalty=0.):
         self.lengths = lengths
         self.dt = dt
-        self.masses=masses
+        self.masses = masses
         self.n = len(masses)
         n = self.n
         m = np.cumsum(self.masses[::-1])[::-1]
         Upp = -9.81 * self.lengths * m
-        self.M = np.outer(self.lengths, self.lengths) * np.minimum(m[:,None], m)
+        self.M = np.outer(
+            self.lengths, self.lengths) * np.minimum(m[:, None], m)
         Minv = np.linalg.pinv(self.M)
         A = np.eye(2 * n)
         A[:n, n:] += np.eye(n) * self.dt
-        A[n:, :n] -= self.dt*Minv*Upp[None,:]
-        B = np.zeros((2*n, n))
-        B[n:,:]+= Minv * self.dt
-        Q = np.zeros((2*n, 2*n))
-        Q[:n, :n] += np.eye(n)*penalty
-        R = np.eye(n)*action_penalty
+        A[n:, :n] -= self.dt * Minv * Upp[None, :]
+        B = np.zeros((2 * n, n))
+        B[n:, :] += Minv * self.dt
+        Q = np.zeros((2 * n, 2 * n))
+        Q[:n, :n] += np.eye(n) * penalty
+        R = np.eye(n) * action_penalty
         mdp.LQRMDP.__init__(self, A, B, Q, R, Sigma=sigma,
-            start=np.zeros(2*n))
+                            start=np.zeros(2 * n))
+
 
 class PoleBalancingMDP(mdp.LQRMDP):
     """
-    Linear Quadratic MDP which models the pole balancing task 
+    Linear Quadratic MDP which models the pole balancing task
     i.e. 2D inverted pendulum linearly approximated around the
     balance point.
-    
+
         S = [\alpha, \dot \alpha, x, \dot x]
         A = [\ddot x]
-        
+
     \alpha is the angle of the pendulum.
-    x is the position of the cart / hand 
+    x is the position of the cart / hand
     """
 
-    def __init__(self, mass=1., length=5., dt= .01, sigma=0.):
+    def __init__(self, mass=1., length=5., dt=.01, sigma=0.):
         """
         mass: point mass of the pendulum
         length: length of the stick / pendulum
@@ -90,66 +93,70 @@ class PoleBalancingMDP(mdp.LQRMDP):
         self.dt = dt
         g = 9.81
         A = np.array([[1., dt, 0, 0],
-                  [g/length, 1, 0 ,0],
-                  [0., 0, 1, dt],
-                  [0, 0, 0, 1]])
-        B = np.array([0., dt/length, 0, dt]).reshape(4,1)
+                      [g / length, 1, 0, 0],
+                      [0., 0, 1, dt],
+                      [0, 0, 0, 1]])
+        B = np.array([0., dt / length, 0, dt]).reshape(4, 1)
         Q = np.diag([-100., 0., -1, 0])
         #terminal_f = lambda x: np.abs(x[0]) > 10
-        
-        R = np.ones((1,1))*(-0.1)
 
-        mdp.LQRMDP.__init__(self, A, B, Q, R, np.array([0.0001, 0, 0, 0]), sigma)
-                            
+        R = np.ones((1, 1)) * (-0.1)
+
+        mdp.LQRMDP.__init__(
+            self, A, B, Q, R, np.array([0.0001, 0, 0, 0]), sigma)
+
     def animate_trace(self, state_trace, action_trace=None):
         fig = plt.figure()
-        off = np.max(np.abs(state_trace[:,2])) + self.length
-        ax = fig.add_subplot(111, autoscale_on=False, xlim=(-off, off), ylim=(-1, self.length *1.1))
+        off = np.max(np.abs(state_trace[:, 2])) + self.length
+        ax = fig.add_subplot(111, autoscale_on=False, xlim=(-off,
+                             off), ylim=(-1, self.length * 1.1))
         ax.grid()
         line, = ax.plot([], [], 'o-', lw=2)
         time_template = 'time = %.1fs'
-        time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)    
-        
+        time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
         psi_template = r'$\psi$ = %.2g'
         psi_text = ax.text(0.05, 0.8, '', transform=ax.transAxes)
         if action_trace is not None:
             line_a, = ax.plot([], [], 'r-*', lw=2)
-        
+
         def init():
             line.set_data([], [])
             time_text.set_text('')
             psi_text.set_text('')
-            
+
             if action_trace is not None:
                 line_a.set_data([], [])
                 return line, line_a, time_text, psi_text
             return line, time_text, psi_text
 
         def anim(i):
-            thisx = [state_trace[i,2],state_trace[i,2] + self.length * np.sin(10*state_trace[i,0])]
-            thisy = [0,self.length * np.cos(10*state_trace[i,0])]
+            thisx = [state_trace[i, 2], state_trace[i, 2] +
+                     self.length * np.sin(10 * state_trace[i, 0])]
+            thisy = [0, self.length * np.cos(10 * state_trace[i, 0])]
 
             line.set_data(thisx, thisy)
-            time_text.set_text(time_template%(i*self.dt))
-            
-            psi_text.set_text(psi_template%(state_trace[i,0]))
-            if action_trace is not None:
-                line_a.set_data([0., action_trace[i,0]/np.max(np.abs(action_trace))], [-0.9, -0.9])
-                return line, line_a, time_text, psi_text
-            return line, time_text, psi_text    
-        
+            time_text.set_text(time_template % (i * self.dt))
 
-        ani = animation.FuncAnimation(fig, anim, np.arange(state_trace.shape[0]),
-                                      interval=1000.*self.dt*10, blit=True, init_func=init)
+            psi_text.set_text(psi_template % (state_trace[i, 0]))
+            if action_trace is not None:
+                line_a.set_data([0., action_trace[i, 0] /
+                                np.max(np.abs(action_trace))], [-0.9, -0.9])
+                return line, line_a, time_text, psi_text
+            return line, time_text, psi_text
+
+        ani = animation.FuncAnimation(
+            fig, anim, np.arange(state_trace.shape[0]),
+            interval=1000. * self.dt * 10, blit=True, init_func=init)
         return ani
 
 
 class RandomMDP(mdp.MDP):
-    """ 
-    Random MDP with uniformly distributed transition probabilities and
-    reward function.    
     """
-    
+    Random MDP with uniformly distributed transition probabilities and
+    reward function.
+    """
+
     def __init__(self, n_states, seed=None):
         """
             n_states: number of states
@@ -157,17 +164,17 @@ class RandomMDP(mdp.MDP):
         """
         if seed is not None:
             np.random.seed(seed)
-        n_s = n_states        
+        n_s = n_states
         actions = [0, ]
         states = range(n_s)
-        
+
         d0 = np.random.rand(n_s) + 1e-5
         d0 = d0 / d0.sum()
-        
+
         r = np.random.rand(n_s, 1, n_s)
         r[:, :, n_s - 1] = 1
         P = np.random.rand(n_s, 1, n_s) + 1e-5
-        P /= P.sum(axis=2)[:,:,np.newaxis]
+        P /= P.sum(axis=2)[:, :, np.newaxis]
 
         mdp.MDP.__init__(self, states, actions, r, P, d0)
 
@@ -192,23 +199,24 @@ class PendulumSwingUpCartPole(mdp.ContinuousMDP):
         angular position of the pendulum  (in [-pi, +pi[)
 
     """
-    def __init__(self, M=0.5, l=0.6, m=0.5 ,dt=0.15, b=0.1, Sigma=0.):
+    def __init__(self, M=0.5, l=0.6, m=0.5, dt=0.15, b=0.1, Sigma=0.):
         self.l = l
         self.M = M
         self.m = m
         self.dt = dt
         self.b = b
 
-        mdp.ContinuousMDP.__init__(self, self.statefun, self.rewardfun, 4, 1, self.__class__.randstart(), Sigma=Sigma)
+        mdp.ContinuousMDP.__init__(self, self.statefun, self.rewardfun, 4,
+                                   1, self.__class__.randstart(), Sigma=Sigma)
 
     def __repr__(self):
-        return "<PendulumSwingUpCartPole("+repr([self.l, self.M, self.m, self.dt, self.b, self.Sigma])+")>"
+        return "<PendulumSwingUpCartPole(" + repr([self.l, self.M, self.m, self.dt, self.b, self.Sigma]) + ")>"
 
     @staticmethod
     def randstart():
-        return np.array([0., 0., 0., (np.random.rand()-.5)*2*np.pi])
+        return np.array([0., 0., 0., (np.random.rand() - .5) * 2 * np.pi])
 
-    def ode(self,s,t,a):
+    def ode(self, s, t, a):
         g = 9.81
         m = self.m
         l = self.l
@@ -216,22 +224,23 @@ class PendulumSwingUpCartPole(mdp.ContinuousMDP):
         b = self.b
         ds = np.zeros(4)
         ds[0] = s[1]
-        ds[1] = (2*m*l*s[2]**2 * np.sin(s[3]) + 3*m*g*np.sin(s[3])*np.cos(s[3])+ 4*a-4*b*s[1])\
-        / (4*(M+m)-3*m*np.cos(s[3])**2)
-        ds[2] = (-3*m*l*s[2]**2*np.sin(s[3])*np.cos(s[3])-6*(M+m)*g*np.sin(s[3])-6*(a-b*s[1])*np.cos(s[3]))\
-        / (4*l*(m+M)-3*m*l*np.cos(s[3])**2)
+        ds[1] = (2 * m * l * s[2] ** 2 * np.sin(s[3]) + 3 * m * g * np.sin(s[3]) * np.cos(s[3]) + 4 * a - 4 * b * s[1])\
+            / (4 * (M + m) - 3 * m * np.cos(s[3]) ** 2)
+        ds[2] = (-3 * m * l * s[2] ** 2 * np.sin(s[3]) * np.cos(s[3]) - 6 * (M + m) * g * np.sin(s[3]) - 6 * (a - b * s[1]) * np.cos(s[3]))\
+            / (4 * l * (m + M) - 3 * m * l * np.cos(s[3]) ** 2)
         ds[3] = s[2]
         return ds
 
     def statefun(self, s, a):
-        s1 = scipy.integrate.odeint(self.ode,s,[0., self.dt], args=(a,), printmessg=False)
-        s1 = s1[-1,:].flatten()
-        s1[-1] = ((s1[-1] + np.pi) % (2*np.pi)) - np.pi
+        s1 = scipy.integrate.odeint(
+            self.ode, s, [0., self.dt], args=(a,), printmessg=False)
+        s1 = s1[-1, :].flatten()
+        s1[-1] = ((s1[-1] + np.pi) % (2 * np.pi)) - np.pi
         return s1
 
     def rewardfun(self, s, a):
         l = self.l
-        return -np.cos(s[-1])*l - 1e-5 *np.abs(s[0])
+        return -np.cos(s[-1]) * l - 1e-5 * np.abs(s[0])
 
     def __getstate__(self):
         res = mdp.ContinuousMDP.__getstate__(self)
@@ -246,46 +255,49 @@ class PendulumSwingUpCartPole(mdp.ContinuousMDP):
 
     def animate_trace(self, state_trace, action_trace=None):
         fig = plt.figure()
-        off = np.max(np.abs(state_trace[:,0])) + self.l
-        ax = fig.add_subplot(111, autoscale_on=False, aspect='equal', 
-                             xlim=(-off, off), ylim=(-self.l *2, self.l *2))
+        off = np.max(np.abs(state_trace[:, 0])) + self.l
+        ax = fig.add_subplot(111, autoscale_on=False, aspect='equal',
+                             xlim=(-off, off), ylim=(-self.l * 2, self.l * 2))
         ax.grid()
         line, = ax.plot([], [], 'o-', lw=2)
         time_template = 'time = %.1fs'
-        time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)    
-        
+        time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
         psi_template = r'$\psi$ = %.2g'
         psi_text = ax.text(0.05, 0.8, '', transform=ax.transAxes)
         if action_trace is not None:
             line_a, = ax.plot([], [], 'r-*', lw=2)
-        
+
         def init():
             line.set_data([], [])
             time_text.set_text('')
             psi_text.set_text('')
-            
+
             if action_trace is not None:
                 line_a.set_data([], [])
                 return line, line_a, time_text, psi_text
             return line, time_text, psi_text
 
         def anim(i):
-            thisx = [state_trace[i,0],state_trace[i,0] + self.l * np.sin(state_trace[i,3])]
-            thisy = [0,- self.l * np.cos(state_trace[i,3])]
+            thisx = [state_trace[i, 0], state_trace[i, 0] +
+                     self.l * np.sin(state_trace[i, 3])]
+            thisy = [0, - self.l * np.cos(state_trace[i, 3])]
 
             line.set_data(thisx, thisy)
-            time_text.set_text(time_template%(i*self.dt))
-            
-            psi_text.set_text(psi_template%(state_trace[i,3]))
-            if action_trace is not None:
-                line_a.set_data([0., action_trace[i,0]/np.max(np.abs(action_trace))], [-0.9, -0.9])
-                return line, line_a, time_text, psi_text
-            return line, time_text, psi_text    
-        
+            time_text.set_text(time_template % (i * self.dt))
 
-        ani = animation.FuncAnimation(fig, anim, np.arange(state_trace.shape[0]),
-                                      interval=1000.*self.dt, blit=True, init_func=init)
+            psi_text.set_text(psi_template % (state_trace[i, 3]))
+            if action_trace is not None:
+                line_a.set_data([0., action_trace[i, 0] /
+                                np.max(np.abs(action_trace))], [-0.9, -0.9])
+                return line, line_a, time_text, psi_text
+            return line, time_text, psi_text
+
+        ani = animation.FuncAnimation(
+            fig, anim, np.arange(state_trace.shape[0]),
+            interval=1000. * self.dt, blit=True, init_func=init)
         return ani
+
 
 class RandomWalkChain(mdp.MDP):
     """ Random Walk chain example MDP """
@@ -328,11 +340,11 @@ class RandomWalkChain(mdp.MDP):
         res = res.astype("float")
         res /= np.sqrt(np.sum(res))
         return res
-        
-    def linear_phi(self, state):       
+
+    def linear_phi(self, state):
         n = len(self.states)
         a = (n - 1.) / (self.n_feat - 1)
-        r = 1 - abs((state + 1 - np.linspace(1,n,self.n_feat)) / a)
+        r = 1 - abs((state + 1 - np.linspace(1, n, self.n_feat)) / a)
         r[r < 0] = 0
         return r
 
@@ -341,11 +353,11 @@ class BoyanChain(mdp.MDP):
     """
     Boyan Chain example. All states form a chain with ongoing arcs and a
     single terminal state. From a state one transition with equal probability
-    to either the direct or the second successor state. All transitions have 
-    a reward of -3 except going from second to last to last state (-2)    
-    """        
+    to either the direct or the second successor state. All transitions have
+    a reward of -3 except going from second to last to last state (-2)
+    """
 
-    def __init__(self, n_states, n_feat):
+    def __init__(self, n_states, n_feat, additional_terminal=False):
         """
             n_states: number of states including terminal ones
             n_feat: number of features used to represent the states
@@ -353,31 +365,42 @@ class BoyanChain(mdp.MDP):
         """
         assert n_states >= n_feat
         #assert (n_states - 1) % (n_feat - 1) == 0
-        n_s = n_states
-        self.n_feat = n_feat 
+        n_s = n_states if not additional_terminal else n_states + 1
+        self.n_feat = n_feat
         states = range(n_s)
         actions = [0, ]
         d0 = np.zeros(n_s)
         d0[0] = 1
-        r = np.ones((n_s, 1, n_s))*(-3)
-        r[-2, :, -1] = -2
-        r[-1,:,-1] = 0
+        r = np.ones((n_s, 1, n_s)) * (-3)
+        r[n_states - 2, :, n_states - 1] = -2
+        r[n_states - 1:, :, :] = 0
         P = np.zeros((n_s, 1, n_s))
         P[-1, :, -1] = 1
-        P[-2, :, -1] = 1
-        for s in np.arange(n_s - 2):
+        if additional_terminal:
+            P[-2, :, -2] = 4 / 5.
+            P[-2, :, -1] = 1 / 5.
+        P[n_states - 2, :, n_states - 1] = 1
+        for s in np.arange(n_states - 2):
             P[s, :, s + 1] = 0.5
             P[s, :, s + 2] = 0.5
 
         mdp.MDP.__init__(self, states, actions, r, P, d0)
-        
-    def phi(self, state):       
+
+    def phi(self, state):
         n = len(self.states)
         a = (n - 1.) / (self.n_feat - 1)
-        r = 1 - abs((state + 1 - np.linspace(1,n,self.n_feat)) / a)
+        r = 1 - abs((state + 1 - np.linspace(1, n, self.n_feat)) / a)
         r[r < 0] = 0
         return r
 
+    def phi_sp(self, state):
+        if state == self.states[-1]:
+            state = state -1
+        n = len(self.states)-1
+        a = (n - 1.) / (self.n_feat - 1)
+        r = 1 - abs((state + 1 - np.linspace(1, n, self.n_feat)) / a)
+        r[r < 0] = 0
+        return r
 
 class BairdStarExample(mdp.MDP):
     """
@@ -406,7 +429,4 @@ class BairdStarExample(mdp.MDP):
         d0 = np.ones((n_s), dtype="double") / n_s
 
         mdp.MDP.__init__(self, range(1, n_corners + 1) + ["center", ],
-                     actions, r, P, d0)
-
-
-
+                         actions, r, P, d0)

experiments/gradient_descent.py
@@ -19,59 +19,57 @@ import scipy.optimize
 
 n = 7
 n_feat = 2
-n_iter = 100
-mdp = examples.BoyanChain(n, n_feat)
-phi=mdp.phi
-task = task.LinearDiscreteValuePredictionTask(mdp, 1, phi, np.array([50.,0.]))
+n_iter = 50
+mdp = examples.BoyanChain(n, n_feat, additional_terminal=False)
+phi = mdp.phi
+task = task.LinearDiscreteValuePredictionTask(mdp, 0.9, phi, np.array([50., -20.]))
 
 
 methods = []
 
-lstd = td.LSTDLambda(lam=0, eps=1000, phi=phi)
-lstd.name = r"LSTD({}) $\epsilon$={}".format(0, 1000)    
-lstd.color = "b"        
-#methods.append(lstd)
 
 for alpha in [1]:
-    td0 = td.LinearTDLambda(lam=0, alpha=alpha, phi=phi)
-    td0.name = r"TD(0) $\alpha$={}".format(alpha)    
+    td0 = td.LinearTD0(alpha=alpha, phi=phi)
+    td0.name = r"TD(0) $\alpha$={}".format(alpha)
     td0.color = "k"
     methods.append(td0)
-    
-for alpha, mu in[( 1, 0.1)]:        
-    tdc = td.TDC(alpha=alpha, beta=alpha*mu, phi=phi)
-    tdc.name = r"TDC $\alpha$={} $\mu$={}".format(alpha, mu)   
-    tdc.color = "r"        
+
+for alpha, mu in[(1, 0.1)]:
+    tdc = td.TDC(alpha=alpha, beta=alpha * mu, phi=phi)
+    tdc.name = r"TDC $\alpha$={} $\mu$={}".format(alpha, mu)
+    tdc.color = "r"
     methods.append(tdc)
 
 err_f = task._init_error_fun("RMSE")
-param_sgd = task.parameter_traces(methods, n_samples=500)
+param_sgd = task.parameter_traces(methods, n_samples=150)
 
 # Ordinary Gradient Descent
-grad = lambda x: scipy.optimize.approx_fprime(x, err_f, scipy.optimize.optimize._epsilon*5)
-param_gd = np.empty((param_sgd.shape[0],2))
+grad = lambda x: scipy.optimize.approx_fprime(
+    x, err_f, scipy.optimize.optimize._epsilon * 5)
+param_gd = np.empty((param_sgd.shape[0], 2))
 p = task.theta0
 for i in xrange(param_gd.shape[0]):
-    param_gd[i,:] = p
-    p -= 10*grad(p)
+    param_gd[i, :] = p
+    p -= 10 * grad(p)
 
-size = np.array(param_sgd.shape) + [0,1,0]
+size = np.array(param_sgd.shape) + [0, 1, 0]
 param = np.empty(size)
-param[:,:-1,:] = param_sgd
-param[:,-1,:] = param_gd
+param[:, :-1, :] = param_sgd
+param[:, -1, :] = param_gd
 
 
-w1, w2 = np.meshgrid(np.linspace(np.min(-10 + param[:,:,0]), 10 + np.max(param[:,:,0]), 150), 
-                     np.linspace(np.min(-10 + param[:,:,1]), 10 + np.max(param[:,:,1]), 150))
-                     
+w1, w2 = np.meshgrid(
+    np.linspace(
+        np.min(-10 + param[:, :, 0]), 10 + np.max(param[:, :, 0]), 150),
+    np.linspace(np.min(-10 + param[:, :, 1]), 10 + np.max(param[:, :, 1]), 150))
 
-extends = np.min(w1), np.max(w1), np.min(w2),np.max(w2)
+
+extends = np.min(w1), np.max(w1), np.min(w2), np.max(w2)
 s = w1.copy()
 
 for i in xrange(w1.shape[0]):
     for j in xrange(w2.shape[1]):
-        s[i,j] = err_f([w1[i,j], w2[i,j]])
-
+        s[i, j] = err_f([w1[i, j], w2[i, j]])
 
 
 plt.figure()
@@ -81,10 +79,10 @@ plt.ylabel(r"$\theta_2$")
 plt.autoscale(tight=True)
 plt.colorbar().ax.set_ylabel(r"$\sqrt{MSE}$")
 
-plt.plot(param[:,0,0],param[:,0,1],'w', linewidth=2, label="TD(0)")
-plt.plot(param[:,1,0],param[:,1,1],'#ff2277', linewidth=2, label="TDC")
-plt.plot(param[:,2,0],param[:,2,1],'m', linewidth=2, label="Grad. Desc.")
-lg = plt.legend(loc= "lower right")    
+plt.plot(param[:, 0, 0], param[:, 0, 1], 'w', linewidth=2, label="TD(0)")
+plt.plot(param[:, 1, 0], param[:, 1, 1], '#ff2277', linewidth=2, label="TDC")
+plt.plot(param[:, 2, 0], param[:, 2, 1], 'm', linewidth=2, label="Grad. Desc.")
+lg = plt.legend(loc="lower right")
 lg.get_frame().set_facecolor('#cccccc')
 
 plt.show()

task.py
@@ -11,7 +11,7 @@ import dynamic_prog
 import numpy as np
 from util.progressbar import ProgressBar
 import util
-from joblib import delayed, Parallel
+from joblib import Parallel
 #import matplotlib.pyplot as plt
 import policies
 import features
@@ -153,9 +153,8 @@ class LinearValuePredictionTask(object):
 
         return param
 
-    def parameter_traces(self, methods, n_samples=1000, seed=None):
+    def parameter_traces(self, methods, n_samples=1000, seed=None, override_terminal=0):
         # deprecated
-        pass
         self._init_methods(methods)
 
         param = np.empty((n_samples, len(methods)) + self.theta0.shape)
@@ -169,9 +168,11 @@ class LinearValuePredictionTask(object):
             for s, a, s_n, r in self.mdp.sample_transition(
                 n_samples, policy=self.behavior_policy,
                 with_restart=False,
-                    seed=cur_seed):
+                seed=cur_seed,
+                    override_terminal=override_terminal):
                 f0 = self.phi(s)
                 f1 = self.phi(s_n)
+                #print s, a, s_n, r, f0, f1
                 for k, m in enumerate(methods):
                     if self.off_policy:
                         m.update_V_offpolicy(s, s_n, r, a,