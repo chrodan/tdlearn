bugfix mu_phi_next computation

features.py
@@ -13,6 +13,7 @@ class gaussians(object):
         return phi / np.sum(phi)
 
     def __init__(self, means, sigmas):
+        self.dim = means.shape[0]
         assert(means.shape[0] == sigmas.shape[0])
         self.means = means
         self.sigmasq = np.power(sigmas, 2)
@@ -39,7 +40,7 @@ class linear_blended(object):
     def __call__(self, x):
         state = int(x)
         n_corners = self.n_states - 1
-        result = np.zeros(n_corners + 2)
+        result = np.zeros(self.dim)
         if state == n_corners:
             result[-1] = 2
             result[-2] = 1
@@ -49,6 +50,7 @@ class linear_blended(object):
         return result
 
     def __init__(self, n_states):
+        self.dim = n_states + 1
         self.n_states = n_states
 
 
@@ -57,7 +59,8 @@ class squared_full(object):
     def __repr__(self):
         return "squared_full(" + repr(self.normalization) + ")"
 
-    def __init__(self, normalization=None):
+    def __init__(self, dim, normalization=None):
+        self.dim = dim
         self.normalization = normalization
 
     def __call__(self, state):
@@ -97,7 +100,8 @@ class squared_tri(object):
     def __repr__(self):
         return "squared_tri(" + repr(self.normalization) + ")"
 
-    def __init__(self, normalization=None):
+    def __init__(self, dim, normalization=None):
+        self.dim = dim
         self.normalization = normalization
 
     def __call__(self, state):
@@ -150,7 +154,8 @@ class squared_diag(object):
     def __repr__(self):
         return "squared_diag(" + repr(self.normalization) + ")"
 
-    def __init__(self, normalization=None):
+    def __init__(self, dim, normalization=None):
+        self.dim = dim
         self.normalization = normalization
 
     def __call__(self, state):

mdp.py
@@ -7,7 +7,7 @@ Created on Fri Dec  9 19:01:41 2011
 @author: Christoph Dann <cdann@cdann.de>
 """
 import numpy as np
-from util import multinomial_sample, memory
+from util import multinomial_sample, memory, apply_rowise
 
 
 def _false(x):
@@ -42,26 +42,30 @@ def samples_cached(mymdp, policy, n_iter=1000, n_restarts=100,
 
 
 @memory.cache(hashfun={"mymdp": repr, "policy": repr})
-def samples_distribution(mymdp, policy, n_iter=1000, n_restarts=100, n_next=20, seed=1):
-    states = np.ones([n_restarts * n_next * n_iter, mymdp.dim_S])
-    states_next = np.ones([n_restarts * n_next * n_iter, mymdp.dim_S])
-    actions = np.ones([n_restarts * n_next * n_iter, mymdp.dim_A])
-    rewards = np.ones(n_restarts * n_next * n_iter)
+def samples_distribution(mymdp, policy, phi, n_subsample=1, n_iter=1000, n_restarts=100, n_next=20, seed=1):
+    assert(n_subsample == 1)  # not implemented, do that if you need it
+    states = np.ones([n_restarts * n_iter, mymdp.dim_S])
+    states_next = np.ones([n_restarts * n_iter, mymdp.dim_S])
+    feat = np.zeros((n_restarts * n_iter, phi.dim))
+    feat_next = np.zeros_like(feat)
+    rewards = np.ones(n_restarts * n_iter)
     np.random.seed(seed)
 
     k = 0
     s = mymdp.start()
-    for k in xrange(0, n_restarts * n_iter * n_next, n_next):
+    for k in xrange(n_restarts * n_iter):
         if mymdp.terminal_f(s):
             s = mymdp.start()
         s0, a, s1, r = mymdp.sample_step(s, policy=policy, n_samples=n_next)
-        states[k:k + n_next, :] = s0
-        states_next[k:k + n_next, :] = s1
-        rewards[k:k + n_next] = r
-        actions[k:k + n_next, :] = a
-        s = s1[-1, :]
+        states[k, :] = s0
+        feat[k, :] = phi(s0)
+        fn = apply_rowise(phi, s1)
+        feat_next[k, :] = np.mean(fn, axis=0)
+        states_next[k, :] = np.mean(s1, axis=0)
+        rewards[k] = np.mean(r)
+        s = np.mean(s1, axis=0)
 
-    return states, actions, rewards, states_next
+    return states, rewards, states_next, feat, feat_next
 
 
 class ContinuousMDP(object):
@@ -131,12 +135,12 @@ class ContinuousMDP(object):
 
         n_feat = len(phi(np.zeros(self.dim_S)))
         feats = np.empty(
-            [int(n_restarts * n_iter * n_next / float(n_subsample)), n_feat])
+            [int(n_restarts * n_iter / float(n_subsample)), n_feat])
         feats_next = np.empty(
-            [int(n_restarts * n_iter * n_next / float(n_subsample)), n_feat])
+            [int(n_restarts * n_iter / float(n_subsample)), n_feat])
         i = 0
         l = range(0, n_restarts * n_iter * n_next, n_subsample)
-        for k in xrange(n_iter * n_restarts * n_next):
+        for k in xrange(n_iter * n_restarts):
             if k % n_subsample == 0:
                 feats[i, :] = phi(s[k])
                 feats_next[i, :] = phi(sn[k])
@@ -204,12 +208,11 @@ class ContinuousMDP(object):
             s1 = mean + rands.flatten()
             r = self.rf(s0, a)
         else:
-            s0 = np.ones((n_samples, self.dim_S)) * s0[None, :]
             s1 = np.zeros((n_samples, self.dim_S))
             r = np.zeros(n_samples)
             for i in xrange(n_samples):
-                s1[i, :] = self.sf(s0[i], a[i])
-                r = self.rf(s0[i], a[i])
+                s1[i, :] = self.sf(s0, a[i])
+                r = self.rf(s0, a[i])
             s1 += rands
         return (s0, a, s1, r)
 
@@ -398,7 +401,7 @@ class MDP(object):
     def samples_cached(self, policy, n_iter=1000, n_restarts=100,
                        no_next_noise=False, seed=1):
 
-        assert (no_next_noise == False)
+        assert (not no_next_noise)
         assert(seed is not None)
         states = np.ones([n_restarts * n_iter, self.dim_S])
         states_next = np.ones([n_restarts * n_iter, self.dim_S])

task.py
@@ -12,10 +12,9 @@ import numpy as np
 from util.progressbar import ProgressBar
 import util
 from joblib import Parallel
-#import matplotlib.pyplot as plt
 import policies
 import features
-from util import memory
+import mdp
 
 
 def tmp(cl, *args, **kwargs):
@@ -169,7 +168,7 @@ class LinearValuePredictionTask(object):
             for s, a, s_n, r in self.mdp.sample_transition(
                 n_samples, policy=self.behavior_policy,
                 with_restart=False,
-                seed=cur_seed):
+                    seed=cur_seed):
                     #override_terminal=override_terminal):
                 f0 = self.phi(s)
                 f1 = self.phi(s_n)
@@ -546,13 +545,13 @@ class LinearContinuousValuePredictionTask(LinearValuePredictionTask):
         are very costly to compute, so they are only evaluated, if really needed
         """
         if name == "mu" or name == "mu_next" or name == "mu_r" or name == "mu_phi" or name == "mu_phi_next":
-            self.mu, _, self.mu_r, self.mu_next, self.mu_phi, self.mu_phi_next = self.mdp.samples_featured(policy=self.target_policy,
-                                                                                                           phi=self.phi,
-                                                                                                           n_next=self.mu_n_next,
-                                                                                                           n_iter=self.mu_iter,
-                                                                                                           n_restarts=self.mu_restarts,
-                                                                                                           seed=self.mu_seed,
-                                                                                                           n_subsample=self.mu_subsample)
+            self.mu, self.mu_r, self.mu_next, self.mu_phi, self.mu_phi_next = mdp.samples_distribution(self.mdp, policy=self.target_policy,
+                                                                                                       phi=self.phi,
+                                                                                                       n_next=self.mu_n_next,
+                                                                                                       n_iter=self.mu_iter,
+                                                                                                       n_restarts=self.mu_restarts,
+                                                                                                       seed=self.mu_seed,
+                                                                                                       n_subsample=self.mu_subsample)
             return self.__dict__[name]
 
         else:
@@ -588,7 +587,7 @@ class LinearLQRValuePredictionTask(LinearContinuousValuePredictionTask):
             return self.V_true
         elif name == "mu_phi_full":
             self.mu_phi_full = util.apply_rowise(
-                features.squared_tri(), self.mu)
+                features.squared_tri(self.mdp.dim_S), self.mu)
             return self.mu_phi_full
         else:
             return LinearContinuousValuePredictionTask.__getattr__(self, name)
@@ -646,18 +645,16 @@ class LinearLQRValuePredictionTask(LinearContinuousValuePredictionTask):
         return Pn, bn
 
     def MSE(self, theta):
-        p = features.squared_tri().param_forward(*self.phi.param_back(theta)) -\
-            features.squared_tri().param_forward(*self.V_true)
+        p = features.squared_tri(self.mdp.dim_S).param_forward(*self.phi.param_back(theta)) -\
+            features.squared_tri(self.mdp.dim_S).param_forward(*self.V_true)
         return np.mean((p * self.mu_phi_full).sum(axis=1) ** 2)
 
     def MSBE(self, theta):
         """ Mean Squared Bellman Error """
         V = np.array((theta * self.mu_phi).sum(axis=1))
-        theta_trans = features.squared_tri().param_forward(
+        theta_trans = features.squared_tri(self.mdp.dim_S).param_forward(
             *self.bellman_operator(*self.phi.param_back(theta)))
         V2 = np.array((theta_trans * self.mu_phi_full).sum(axis=1))
-        #import ipdb
-        #ipdb.set_trace()
         return np.mean((V - V2) ** 2)
 
     def MSPBE(self, theta):
@@ -665,7 +662,7 @@ class LinearLQRValuePredictionTask(LinearContinuousValuePredictionTask):
         """ Mean Squared Projected Bellman Error """
         #print "Wrong!"
         V = np.matrix((theta * np.asarray(self.mu_phi)).sum(axis=1)).T
-        theta_trans = features.squared_tri().param_forward(
+        theta_trans = features.squared_tri(self.mdp.dim_S).param_forward(
             *self.bellman_operator(*self.phi.param_back(theta)))
         v = np.asarray(V - self.projection_operator(
         ) * np.matrix(self.mu_phi_full) * np.matrix(theta_trans).T)